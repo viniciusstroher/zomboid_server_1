fdd8c9be8b506f58f5bc8aafefa8d04db432bccf
Veni <vinicius@socialsome.com.br>
2026-08-05 13:23:38 -0300

    Update spawn points in venizao_spawnpoints.lua with new coordinates and regions, and modify ResetID in venizao.ini for server consistency, enhancing gameplay experience.

--- config/venizao_spawnpoints.lua
+++ config/venizao_spawnpoints.lua
@@ -1,30 +1,35 @@
 function SpawnPoints()
 	return {
         unemployed = {
-            { worldX = 45, worldY = 10, posX = 82,  posY = 254, posZ = 0 },
-            { worldX = 42, worldY = 11, posX = 224, posY = 253, posZ = 0 },
-            { worldX = 44, worldY = 5,  posX = 273, posY = 241, posZ = 0 },
-            { worldX = 45, worldY = 5,  posX = 29,  posY = 13,  posZ = 3 },
-            { worldX = 40, worldY = 6,  posX = 198, posY = 145, posZ = 0 },
-            { worldX = 40, worldY = 9,  posX = 24,  posY = 130, posZ = 1 },
-            { worldX = 42, worldY = 12, posX = 138, posY = 279, posZ = 0 },
-            { worldX = 41, worldY = 7,  posX = 133, posY = 53,  posZ = 0 },
-            { worldX = 42, worldY = 5,  posX = 248, posY = 79,  posZ = 1 },
-            { worldX = 43, worldY = 5,  posX = 55,  posY = 194, posZ = 1 },
-            { worldX = 46, worldY = 7,  posX = 266, posY = 49,  posZ = 0 },
-            { worldX = 45, worldY = 12, posX = 145, posY = 280, posZ = 0 },
+            
+            { worldX = 41, worldY = 7, posX = 154, posY = 201, posZ = 0 },
+            { worldX = 41, worldY = 7, posX = 205, posY = 143, posZ = 0 },
+            { worldX = 41, worldY = 8, posX = 82,  posY = 104, posZ = 0 },
+            { worldX = 41, worldY = 8, posX = 177, posY = 231, posZ = 0 },
 
-            -- Hospital
-            { worldX = 43, worldY = 6, posX = 45, posY = 237, posZ = 0 },
+            -- Centro-oeste residencial
+            { worldX = 42, worldY = 7, posX = 88,  posY = 180, posZ = 0 },
+            { worldX = 42, worldY = 7, posX = 211, posY = 96,  posZ = 0 },
+            { worldX = 42, worldY = 8, posX = 154, posY = 122, posZ = 0 },
+            { worldX = 42, worldY = 8, posX = 233, posY = 205, posZ = 0 },
 
-            -- Bairros residenciais
+            -- Centro
             { worldX = 43, worldY = 7, posX = 95,  posY = 120, posZ = 0 },
-            { worldX = 44, worldY = 7, posX = 180, posY = 160, posZ = 0 },
-            { worldX = 44, worldY = 8, posX = 60,  posY = 90,  posZ = 0 },
-            { worldX = 45, worldY = 8, posX = 140, posY = 70,  posZ = 0 },
-            { worldX = 41, worldY = 8, posX = 250, posY = 180, posZ = 0 },
-            { worldX = 42, worldY = 9, posX = 180, posY = 210, posZ = 0 },
-            { worldX = 46, worldY = 8, posX = 100, posY = 150, posZ = 0 },
+            { worldX = 43, worldY = 7, posX = 180, posY = 86,  posZ = 0 },
+            { worldX = 43, worldY = 8, posX = 60,  posY = 170, posZ = 0 },
+            { worldX = 43, worldY = 8, posX = 200, posY = 235, posZ = 0 },
+
+            -- Leste
+            { worldX = 44, worldY = 7, posX = 96,  posY = 145, posZ = 0 },
+            { worldX = 44, worldY = 7, posX = 221, posY = 80,  posZ = 0 },
+            { worldX = 44, worldY = 8, posX = 101, posY = 210, posZ = 0 },
+            { worldX = 44, worldY = 8, posX = 198, posY = 130, posZ = 0 },
+
+            -- Sudeste
+            { worldX = 45, worldY = 8, posX = 120, posY = 180, posZ = 0 },
+            { worldX = 45, worldY = 9, posX = 88,  posY = 210, posZ = 0 },
+            { worldX = 45, worldY = 10, posX = 150, posY = 140, posZ = 0 },
+            { worldX = 45, worldY = 10, posX = 220, posY = 240, posZ = 0 },
         }
     }
 end
