3c4923ba1536dd28134716d21b4d89058e5023e2
Veni <vinicius@socialsome.com.br>
2026-08-05 18:39:51 -0300

    Remove admin-send.sh script for Zomboid server management: eliminate unused command execution functionality within the server container.

--- scripts/admin-send.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-CONTAINER_NAME="zomboid_server"
-CMD="${1:-save}"
-
-docker exec "${CONTAINER_NAME}" bash -c "echo '${CMD}' > /proc/1/fd/0"
-echo "[Admin] Comando '${CMD}' enviado ao servidor."
