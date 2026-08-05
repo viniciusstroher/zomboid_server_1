fc1212818ba680fa70b761246a9f2a2c6fb28f2f
Veni <vinicius@socialsome.com.br>
2026-08-04 22:35:10 -0300

    Update docker-compose.yml to change volume mappings for saves and configuration files, and add new configuration files for sandbox variables, spawn points, and spawn regions for the Project Zomboid server.

---
+++ config/venizao_spawnregions.lua
@@ -0,0 +1,11 @@
+function SpawnRegions()
+	return {
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "Louisville, KY", file = "media/maps/Louisville, KY/spawnpoints.lua" },
+		-- Uncomment the line below to add a custom spawnpoint for this server.
+--		{ name = "Twiggy's Bar", serverfile = "venizao_spawnpoints.lua" },
+	}
+end
